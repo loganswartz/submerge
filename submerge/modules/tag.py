7260e8fd1ec2c00d5a1841a76760184a735d5c38
Logan Swartzendruber <logan.swartzendruber@gmail.com>
2020-02-11 01:10:36 +0000

    Implement basic tagging module; Use pycountry for language codes instead of a custom function; add requirements.txt

---
+++ submerge/modules/tag.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# builtins
+import pathlib
+from concurrent.futures import ThreadPoolExecutor
+import subprocess
+import pycountry
+
+# monkeypatch to handle langdetect returning nonstandard identifiers
+# pycountry.languages.lookup('cmn').alpha_2 = 'zh-cn'
+# pycountry.languages.lookup('yue').alpha_2 = 'zh-tw'
+
+
+def language(string):
+    try:
+        lang = pycountry.languages.lookup(string)
+        return lang
+    except LookupError:
+        raise ValueError
+
+
+class TagOperator(object):
+    def __init__(self, subparser):
+        subparser.add_argument('-l', '--language', help='Language the track will be set to', type=language, required=True)
+        subparser.add_argument('-t', '--track', help='Track to modify', type=int, required=True)
+        subparser.add_argument('-r', '--recursive', help='Recurse into directories', action='store_true')
+        subparser.add_argument('-s', '--simulate', help='Print out the command to be executed instead of actually executing it', action='store_true')
+        # TODO
+        # subparser.add_argument('-u', '--only-undefined', help='Only modify tracks when they are undefined', action='store_true')
+        # subparser.add_argument('-c', '--confirm', help='Ask for confirmation before processing', action='store_true')
+
+    def process(self, parser):
+        args = parser.parse_args()
+        self.args = args
+        if args.recursive:
+            files = args.dir.rglob('*.mkv')
+        else:
+            files = args.dir.glob('*.mkv')
+
+        with ThreadPoolExecutor() as executor:
+            results = executor.map(lambda file: self._edit_track(file, args.track, language=args.language), files)
+
+        print('All files modified.')
+
+    def _edit_track(self, file, track, **kwargs):
+        cmd = ['mkvpropedit', str(file.expanduser().resolve()), '--edit', f'track:{track}']
+        for key, value in kwargs.items():
+            cmd.extend(['--set', f'{key}={value.alpha_3}'])
+
+        if self.args.simulate:
+            print(cmd)
+            return cmd
+        else:
+            propedit = subprocess.run(cmd, stdout=subprocess.PIPE)
+            return propedit
+
+
+Operator = TagOperator
+
--- submerge/utils.py
+++ submerge/utils.py
@@ -5,74 +5,6 @@ import pathlib
 from math import floor
 
 
-def ISO639_1_to_ISO639_2(langCode: str):
-    """
-    ISO639_1_to_ISO639_2() converts the language code used by langdetect
-    (ISO 639-1 specifically) and converts it to the scheme used in the MKV
-    standard (ISO 639-2). The 'langs' list is contains all the languages
-    supported by langdetect out of the box, but more could be added by creating
-    more language profiles if need be.
-    """
-
-    langs = {
-            "af":"afr",
-            "ar":"ara",
-            "bg":"bul",
-            "bn":"ben",
-            "ca":"cat",
-            "cs":"ces",
-            "cy":"cym",
-            "da":"dan",
-            "de":"deu",
-            "el":"ell",
-            "en":"eng",
-            "es":"spa",
-            "et":"est",
-            "fa":"fas",
-            "fi":"fin",
-            "fr":"fra",
-            "gu":"guj",
-            "he":"heb",
-            "hi":"hin",
-            "hr":"hrv",
-            "hu":"hun",
-            "id":"ind",
-            "it":"ita",
-            "ja":"jpn",
-            "kn":"kan",
-            "ko":"kor",
-            "lt":"lit",
-            "lv":"lav",
-            "mk":"mkd",
-            "ml":"mal",
-            "mr":"mar",
-            "ne":"nep",
-            "nl":"nld",
-            "no":"nor",
-            "pa":"pan",
-            "pl":"pol",
-            "pt":"por",
-            "ro":"ron",
-            "ru":"rus",
-            "sk":"slk",
-            "sl":"slv",
-            "so":"som",
-            "sq":"sqi",
-            "sv":"swe",
-            "sw":"swa",
-            "ta":"tam",
-            "te":"tel",
-            "th":"tha",
-            "tl":"tgl",
-            "tr":"tur",
-            "uk":"ukr",
-            "ur":"urd",
-            "vi":"vie",
-            "zh-cn":"cmn", # this is actually ISO 639-3, might not work
-            "zh-tw":"yue", # this is actually ISO 639-3, might not work
-            }
-    return(langs[langCode])
-
 def pretty_time_delta(seconds):
     _seconds = int(seconds)
     days, _seconds = divmod(_seconds, 86400)
@@ -87,6 +19,7 @@ def pretty_time_delta(seconds):
         return f"{minutes}m {seconds:.1f}s"
     else:
         return f"{seconds:.2f}s"
+
 
 def get_docstring(obj):
     if obj.__doc__:
